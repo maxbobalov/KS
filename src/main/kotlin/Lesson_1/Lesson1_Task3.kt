a9aee0c6c2eca37e0613d2c2964d939ef12cc20c
maxbo <maxbobalov@gmail.com>
2025-04-20 21:30:09 +0300

    KS-1-3

---
+++ src/main/kotlin/Lesson_1/Lesson1_Task3.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_1
+
+fun main(){
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println("0${hour}")
+    println("0${minute}")
+
+    hour = 10
+    minute = 55
+
+    println("${hour}:${minute}")
+ }
