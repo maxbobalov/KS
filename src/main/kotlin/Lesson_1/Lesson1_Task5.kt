181b706d557fb39d350bfdf981b24c219fc15886
maxbo <maxbobalov@gmail.com>
2025-04-21 19:29:00 +0300

    KS-1-5

--- src/main/kotlin/Lesson_1/Lesson1_Task5.kt
+++ src/main/kotlin/Lesson_1/Lesson1_Task5.kt
@@ -2,5 +2,8 @@ package org.example.Lesson_1
 
 fun main(){
 
-
+ var seconds: Short = 6480
+ var minute: Int = seconds/60
+ var hour: Int = minute/60
+ println("0${hour}:${minute % 60}:0${seconds % 60};")
  }
