866e2962fd13c82a48bd187ea2a784ea8c989010
maxbo <maxbobalov@gmail.com>
2025-04-21 19:25:27 +0300

    main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,8 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-
+    var seconds: Short = 6480
+    var minute: Int = seconds/60
+    var hour: Int = minute/60
+    println("0${hour}:${minute % 60}:0${seconds % 60};")
 }
