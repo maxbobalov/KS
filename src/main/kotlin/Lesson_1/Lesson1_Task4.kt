bb1e1d5bc4f7283f8921cd302b1cbf78b120922a
maxbo <maxbobalov@gmail.com>
2025-04-21 18:33:27 +0300

    KS-1-4

---
+++ src/main/kotlin/Lesson_1/Lesson1_Task4.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson_1
+
+fun main() {
+
+  var length : Long = 40868600000L
+  var age: Byte = 27
+  var paryOfDay: Float = 0.075F
+  var seconds: Short = 6480
+  var partOfYear: Double = 2.0547945205479453E-4
+  var apoage: Int = 327000
+
+  println(length)
+  println(age)
+  println(paryOfDay)
+  println(seconds)
+  println(partOfYear)
+  println(apoage)
+ }
