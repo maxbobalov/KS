0a37da1be8f40538a103786c1f4755376610ea0a
maxbo <maxbobalov@gmail.com>
2025-04-21 20:34:12 +0300

    KS-2-3

---
+++ src/main/kotlin/Lesson_2/Lesson2-Task3.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson_2
+
+fun main() {
+
+   val hour: Byte = 9
+   val minute: Byte = 39
+   val time: Int = 457 + minute
+   val timeHour: Int = hour + time / 60
+   val timeMinute: Int = minute + time % 60
+
+   println("Время прибытия поезда = ${timeHour}:${timeMinute}")
+ }
