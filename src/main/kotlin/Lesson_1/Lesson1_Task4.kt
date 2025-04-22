8d11488e2a95bbe392c676f57226e5a71f057736
maxbo <maxbobalov@gmail.com>
2025-04-22 15:45:16 +0300

    KS-1-4

--- src/main/kotlin/Lesson_1/Lesson1_Task4.kt
+++ src/main/kotlin/Lesson_1/Lesson1_Task4.kt
@@ -2,17 +2,17 @@ package org.example.Lesson_1
 
 fun main() {
 
-  var length : Long = 40868600000L
-  var age: Byte = 27
-  var paryOfDay: Float = 0.075F
-  var seconds: Short = 6480
-  var partOfYear: Double = 2.0547945205479453E-4
-  var apoage: Int = 327000
+    val length: Long = 40868600000L
+    val age: Byte = 27
+    val paryOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apoage: Int = 327000
 
-  println(length)
-  println(age)
-  println(paryOfDay)
-  println(seconds)
-  println(partOfYear)
-  println(apoage)
- }
+    println(length)
+    println(age)
+    println(paryOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apoage)
+}
