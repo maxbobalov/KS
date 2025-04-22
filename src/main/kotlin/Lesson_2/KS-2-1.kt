cd5e8610c1e43f8ecca568db3c02df360e520bd3
maxbo <maxbobalov@gmail.com>
2025-04-22 16:23:42 +0300

    KS-1-5

--- src/main/kotlin/Lesson_2/KS-2-1.kt
+++ src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -1,6 +1,13 @@
 package org.example.Lesson_2
 
-fun main(){
-  var result: Float = (3 + 4 + 3 + 5)/4F
-  println(result)
- }
+fun main() {
+
+    val mark1: Float = 3F
+    val mark2: Float = 4F
+    val mark3: Float = 3F
+    val mark4: Float = 5F
+    val students: Int = 4
+
+    val result: Float = (mark1 + mark2 + mark3 + mark4) / students
+    println(result)
+}
