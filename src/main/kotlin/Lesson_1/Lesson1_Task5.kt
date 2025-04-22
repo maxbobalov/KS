a2b86926a6695e4b224cdcaac916c1e5f1bf25da
maxbo <maxbobalov@gmail.com>
2025-04-22 16:03:37 +0300

    KS-1-5

--- src/main/kotlin/Lesson_1/Lesson1_Task5.kt
+++ src/main/kotlin/Lesson_1/Lesson1_Task5.kt
@@ -1,9 +1,16 @@
 package org.example.Lesson_1
 
-fun main(){
+const val c = 60
 
- var seconds: Short = 6480
- var minute: Int = seconds/60
- var hour: Int = minute/60
- println("0${hour}:${minute % 60}:0${seconds % 60};")
- }
+fun main() {
+
+    val seconds: Short = 6480
+    val minute: Int = seconds / c
+    val hour: Int = minute / c
+    val resultMinute = String.format("%02d", minute % c)
+    val resultSecond = String.format("%02d", seconds % c)
+    val resultHour = String.format("%02d", hour)
+
+    println("${resultHour}:${resultMinute}:${resultSecond};")
+
+}
