f7b6ff7aff09f25f532b84d8654bc872e2b5d50b
maxbo <maxbobalov@gmail.com>
2025-04-22 17:39:11 +0300

    KS-2-5

---
+++ src/main/kotlin/Lesson_2/Lesson2_Task5.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson_2
+import kotlin.math.*
+
+fun main() {
+    val number1: Byte = 1
+    val number2: Byte = 12
+
+    val summa = 70000
+    val stavka: Float = 16.7F
+    val stavka100: Float = stavka / 100
+
+    val year: Byte = 20
+    val month: Int = year * 12
+
+    val res1 = (summa * (number1 + stavka100 / number2 ).pow(n = month))
+
+    println(String.format("%.4f",res1))
+}
