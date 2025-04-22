adb52392cb2c863c3cd08c5e0d5c702e3a0f28d5
maxbo <maxbobalov@gmail.com>
2025-04-22 16:36:22 +0300

    KS-2-3

--- src/main/kotlin/Lesson_2/Lesson2-Task3.kt
+++ src/main/kotlin/Lesson_2/Lesson2-Task3.kt
@@ -1,12 +1,14 @@
 package org.example.Lesson_2
 
+const val c = 60
 fun main() {
 
+   val timeTravel: Int = 457
    val hour: Byte = 9
    val minute: Byte = 39
-   val time: Int = 457 + minute
-   val timeHour: Int = hour + time / 60
-   val timeMinute: Int = minute + time % 60
+   val time: Int = timeTravel + minute
+   val timeHour: Int = hour + time / c
+   val timeMinute: Int = minute + time % c
 
    println("Время прибытия поезда = ${timeHour}:${timeMinute}")
 
