34b39665b50305001d01e2d1856d38c74ba5dc24
maxbo <maxbobalov@gmail.com>
2025-04-23 19:06:09 +0300

    KS-2-5

--- src/main/kotlin/Lesson_2/Lesson2_Task5.kt
+++ src/main/kotlin/Lesson_2/Lesson2_Task5.kt
@@ -2,17 +2,17 @@ package org.example.Lesson_2
 import kotlin.math.*
 
 fun main() {
-    val number1: Byte = 1
-    val number2: Byte = 12
 
-    val summa = 70000
+    val mountsOnYears: Byte = 12
+
+    val depositАmount = 70000
     val stavka: Float = 16.7F
     val stavka100: Float = stavka / 100
 
-    val year: Byte = 20
-    val month: Int = year * 12
+    val years: Byte = 20
+    val months: Int = years * mountsOnYears
 
-    val res1 = (summa * (number1 + stavka100 / number2 ).pow(n = month))
+    val DepositRes = (depositАmount * (1 + stavka100 / mountsOnYears ).pow(n = months))
 
-    println(String.format("%.4f",res1))
+    println(String.format("%.4f",DepositRes))
 }
